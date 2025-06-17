37507886264dade3ab77209c7ffa67f066ee6cf2
Jeremy Karlsson <jeremy@barlingshult.se>
2025-06-17 22:26:38 +0200

    Add cors headers

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,5 +1,16 @@
 <?php
 
+// Allow from any origin
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require_once __DIR__ . '/../controllers/products.php';
 
 $productsController = new ProductsController();
